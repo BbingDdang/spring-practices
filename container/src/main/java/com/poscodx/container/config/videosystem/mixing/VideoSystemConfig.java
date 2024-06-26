a93872281d0858600b6638886a07630a4f7d73f2
BbingDdang <ddaa1541@gmail.com>
2024-06-26 15:47:52 +0900

    create thymeleaf

---
+++ container/src/main/java/com/poscodx/container/config/videosystem/mixing/VideoSystemConfig.java
@@ -0,0 +1,14 @@
+package com.poscodx.container.config.videosystem.mixing;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({DVDConfig.class, DVDPlayerConfig.class){
+	public
+}
+	
+
+public class VideoSystemConfig {
+
+}
